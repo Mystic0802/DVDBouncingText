5ad9eecbbf6f534757e52bbadcc35db8badbc521
Nathan Webb <nathanwebb02@outlook.com>
2022-05-22 16:11:10 +0100

    Add project files.

---
+++ DVDBouncingText/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DVDBouncingText
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
